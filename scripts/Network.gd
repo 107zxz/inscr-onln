23b42179022c8214c34a60385732039024685044
107zxz <i107zxz@icloud.com>
2022-07-25 16:12:33 +0930

    Started porting to new UI

--- scripts/Network.gd
+++ scripts/Network.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const VERSION = "v0.0.22"
+const VERSION = "v0.0.23"
 const DEFAULT_PORT = 10567
 
 const MAX_PEERS = 8
--- scripts/singletons/NetworkManager.gd
+++ scripts/singletons/NetworkManager.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node
 
 
 # Declare member variables here. Examples:
