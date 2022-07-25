a098381bea4d6aa031b3f071fbf696fbdab9cd8f
Thomas Robinson <i107zxz@icloud.com>
2022-07-25 23:19:43 +0930

    Initial LHR integration

--- scripts/singletons/TunnelHandler.gd
+++ scripts/singletons/TunnelHandler.gd
@@ -2,28 +2,37 @@ extends Node
 
 var pid = -1
 
+signal recieved_output(line)
+
 func start_tunnel():
 	print("Starting tunnel")
 	
-	pid = OS.execute ("bash", ["-c", "echo \"\" > listing.txt; ssh -R 80:localhost:10567 nokey@localhost.run > listing.txt"], false)
+	pid = OS.execute ("bash", ["-c", "echo \"\" > lhrlog.txt; ssh -R 80:localhost:10567 nokey@localhost.run > lhrlog.txt"], false)
 	
-#	pid = OS.execute("bash", ["-c", "./cont_output.sh > listing.txt"], false)	
+#	pid = OS.execute("bash", ["-c", "./cont_output.sh > lhrlog.txt"], false)	
 	
 	var fiel = File.new()
 	
 	var current_size = 0
 	
-	fiel.open("listing.txt", File.READ)
+	print("Opening lhrlog.txt with err code: ", fiel.open("lhrlog.txt", File.READ))
 	while OS.is_process_running(pid):
 		yield(get_tree().create_timer(1), "timeout")
 		
 		if fiel.get_len() > current_size:
 			print(current_size, " : ", fiel.get_len())
-			print("got input: ", fiel.get_as_text().right(current_size))
+			print("got tunnel output: ", fiel.get_as_text().right(current_size))
+			
+			emit_signal("recieved_output", fiel.get_as_text().right(current_size))
+			
 			current_size = fiel.get_len()
 		
 func kill_tunnel():
 	if pid > 0 and OS.is_process_running(pid):
-		print("Killing tunnel")
+		print("Killing tunnel with pid ", pid)
 		
-		OS.kill(pid)
+		# Because of how pipes work, we need to do this (on linux at least)
+		print("Error code: ", OS.kill(pid+1))
+
+func _exit_tree():
+	kill_tunnel()
