a77b0ded88bb540c6d1d82e1f63d1400a0fb61ff
Thomas Robinson <i107zxz@icloud.com>
2022-08-01 10:50:27 +0930

    Added rulesets and updating

--- scripts/singletons/CardInfo.gd
+++ scripts/singletons/CardInfo.gd
@@ -1,23 +1,44 @@
 extends Node
 
+const VERSION = "0.1.0 DEV"
+
+var all_data = {}
+var ruleset = "unnamed/cached ruleset"
 var all_sigils = []
 var all_cards = []
 var working_sigils = []
 var deck_path = OS.get_user_data_dir() + "/decks/"
-
+var rules_path = OS.get_user_data_dir() + "/gameInfo.json"
 
 func _enter_tree():
 	read_game_info()
 
+func from_game_info_json(content_as_object):
+	all_data = content_as_object
+	
+	all_sigils = all_data["sigils"]
+	all_cards = all_data["cards"]
+	working_sigils = all_data["working_sigils"]
+	
+	if "ruleset" in all_data:
+		ruleset = all_data.ruleset
+
 func read_game_info():
+	
+	# Does a downloaded ruleset exist?
+	var dir = Directory.new()
 	var file = File.new()
-	file.open("res://data/gameInfo.json", File.READ)
+	
+	if dir.file_exists(rules_path):
+		file.open(rules_path, File.READ)
+		print(rules_path)
+	else:
+		print("Downloaded rules not found! Using saved as fallback (this is normal for first use after install)")
+		file.open("res://data/gameInfo.json", File.READ)
+		
 	var file_content = file.get_as_text()
 	var content_as_object = parse_json(file_content)
-	all_sigils = content_as_object["sigils"]
-	all_cards = content_as_object["cards"]
-	working_sigils = content_as_object["working_sigils"]
-
+	from_game_info_json(content_as_object)
 
 func from_name(cName):
 	for card in all_cards:
