45327ff69b695f200c02c8ce45c39bd201cdde78
107zxz <i107zxz@icloud.com>
2022-08-16 17:02:47 +0930

    Reorganise

---
+++ scripts/classes/buffs/BuffEffect.gd
@@ -0,0 +1,15 @@
+# The idea for this is for each card to have an array of buffs
+class_name BuffEffect
+
+# Stacking
+const STACK_MAX = 1
+var stacks = 1
+
+# References
+var fightManager = null
+var slotManager = null
+var isFriendly = null
+var card = null
+
+func handle_event(event: String):
+	pass
---
+++ scripts/classes/sigils/SigilEffect.gd
@@ -0,0 +1,10 @@
+class_name SigilEffect
+
+# References
+var fightManager = null
+var slotManager = null
+var isFriendly = null
+var card = null
+
+func handle_event(event: String):
+	pass
