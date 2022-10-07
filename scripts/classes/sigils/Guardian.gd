790e010ccc6670eb4b73cf2411b9ca363a0f69bc
107zxz <i107zxz@icloud.com>
2022-10-07 21:42:51 +1030

    slight cleanup

--- scripts/classes/sigils/Guardian.gd
+++ scripts/classes/sigils/Guardian.gd
@@ -7,16 +7,15 @@ func handle_event(event: String, params: Array):
 		return
 
 	var oFriendly = params[0].get_parent().get_parent().name == "PlayerSlots"
-	var friendly = card.get_parent().get_parent().name == "PlayerSlots"
 
 	# attached_card_summoned represents the card bearing the sigil being summoned
-	if event == "card_summoned" and friendly != oFriendly:
+	if event == "card_summoned" and isFriendly != oFriendly:
 
 		var slot = params[0].slot_idx()
 
-		if slotManager.is_slot_empty(slotManager.playerSlots[slot] if friendly else slotManager.enemySlots[slot]):
-			var guardians = slotManager.get_friendly_cards_sigil("Guardian") if friendly else slotManager.get_enemy_cards_sigil("Guardian")
+		if slotManager.is_slot_empty(slotManager.playerSlots[slot] if isFriendly else slotManager.enemySlots[slot]):
+			var guardians = slotManager.get_friendly_cards_sigil("Guardian") if isFriendly else slotManager.get_enemy_cards_sigil("Guardian")
 			if guardians:
 				# slotManager.rpc_id(fightManager.opponent, "remote_card_move", guardians[0].slot_idx(), slot, false)
-				guardians[0].move_to_parent(slotManager.playerSlots[slot] if friendly else slotManager.enemySlots[slot])
+				guardians[0].move_to_parent(slotManager.playerSlots[slot] if isFriendly else slotManager.enemySlots[slot])
 		
