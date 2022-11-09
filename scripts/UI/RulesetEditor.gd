c2a24eac01a4cde0eb3e03e8b4efa08176cdaefc
Thomas Robinson <i107zxz@icloud.com>
2022-11-10 00:13:43 +1030

    Ruleset Editor progress

--- scripts/UI/RulesetEditor.gd
+++ scripts/UI/RulesetEditor.gd
@@ -1,22 +1,85 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 onready var tree = get_node("%RulesetTree")
+var cardRoot = null
+var sigilRoot = null
+
+onready var cardDats = get_node("%CardDat").get_children()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	# Fill Sigils
+	var idx = 1
+	for sigil in CardInfo.all_sigils.keys():
+		cardDats[4].add_item(sigil, idx)
+		cardDats[5].add_item(sigil, idx)
+	
 	var root = tree.create_item()
 	
 	root.set_text(0, "Ruleset")
 	
-	var cardRoot = tree.create_item(root)
-	var sigilRoot = tree.create_item(root)
+	cardRoot = tree.create_item(root)
+	sigilRoot = tree.create_item(root)
 	cardRoot.set_text(0, "Cards")
 	sigilRoot.set_text(0, "Sigils")
 	
-	tree.create_item(cardRoot).set_text(0, "Amogus")
+#	cardRoot.collapsed = true
+	sigilRoot.collapsed = true
+#	root.collapsed = true
 	
+	populate_cards()
+	populate_sigils()
+	
+func populate_cards():
+	for card in CardInfo.all_cards:
+		tree.create_item(cardRoot).set_text(0, card.name)
+
+func populate_sigils():
+	for sigil in CardInfo.all_sigils:
+		var i = tree.create_item(sigilRoot)
+		i.set_text(0, sigil)
+#		i.set_text(1, CardInfo.all_sigils[sigil])
+
+func _on_RulesetTree_item_selected():
+	
+	if not tree.get_selected().get_parent() or tree.get_selected().get_parent().get_text(0) != "Cards":
+		return
+	
+	draw_card()
+	
+
+func draw_card():
+	get_node("%CardDat").visible = true
+	get_node("%Options").visible = false
+	
+	var cDat = CardInfo.from_name(tree.get_selected().get_text(0))
+	
+	# TODO
+	cardDats[0].texture = load("res://gfx/pixport/" + cDat.name + ".png")
+	cardDats[1].value = cDat.attack
+	cardDats[2].value = cDat.health
+	cardDats[3].text = cDat.name
+	
+	if "sigils" in cDat:
+		cardDats[4].select(1+CardInfo.all_sigils.keys().find(cDat.sigils[0]))
+		if cDat.sigils.size() > 1:
+			cardDats[5].select(1+CardInfo.all_sigils.keys().find(cDat.sigils[1]))
+		else:
+			cardDats[5].select(0)
+	else:
+		cardDats[4].select(0)
+		cardDats[5].select(0)
+	
+	cardDats[6].value = cDat.blood_cost if "blood_cost" in cDat else 0
+	cardDats[8].value = cDat.bone_cost if "bone_cost" in cDat else 0
+	cardDats[10].value = cDat.energy_cost if "energy_cost" in cDat else 0
+	
+	if "mox_cost" in cDat:
+		cardDats[12].pressed = "Orange" in cDat.mox_cost
+		cardDats[13].pressed = "Green" in cDat.mox_cost
+		cardDats[14].pressed = "Blue" in cDat.mox_cost
+	else:
+		cardDats[12].pressed = false
+		cardDats[13].pressed = false
+		cardDats[14].pressed = false
