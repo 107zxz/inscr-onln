b9feb0aa5bca575d3497fc97e1c1bc70a8d009fc
107zxz <i107zxz@icloud.com>
2022-11-25 15:47:57 +1030

    Dam builder functionality

---
+++ Dam Builder.gd
@@ -0,0 +1,18 @@
+extends SigilEffect
+
+# This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
+func handle_event(event: String, params: Array):
+
+	# attached_card_summoned represents the card bearing the sigil being summoned
+	if event == "card_summoned" and params[0] == card:
+		
+		print("Dam Builder triggered!")
+		
+		var dam_data = CardInfo.from_name("Dam")
+
+		for sid in [card.slot_idx() - 1, card.slot_idx() + 1]:
+			if sid < 0 or sid > 3:
+				continue
+
+			if slotManager.is_slot_empty(sid):
+				slotManager.summon_card(dam_data, sid, isFriendly) 
