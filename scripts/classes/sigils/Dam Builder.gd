ba46590c23fc3e7d2498705872f0be150cd1cddf
107zxz <i107zxz@icloud.com>
2022-12-10 10:45:19 +1030

    Dam builder and evolve animation fix

---
+++ scripts/classes/sigils/Dam Builder.gd
@@ -0,0 +1,18 @@
+extends SigilEffect
+
+# This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
+func handle_event(event: String, params: Array):
+
+	# attached_card_summoned represents the card bearing the sigil being summoned
+	if event == "card_summoned" and params[0] == card:
+		
+		print("Dam builder triggered!")
+		
+		var cardSlots = slotManager.playerSlots if isFriendly else slotManager.enemySlots
+		var slot = card.slot_idx()
+		
+		if slot > 0 and slotManager.is_slot_empty(cardSlots[slot - 1]):
+			slotManager.summon_card(CardInfo.from_name("Dam"), slot - 1, isFriendly)
+
+		if slot < 3 and slotManager.is_slot_empty(cardSlots[slot + 1]):
+			slotManager.summon_card(CardInfo.from_name("Dam"), slot + 1, isFriendly)
