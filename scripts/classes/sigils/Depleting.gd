f97abec8b793ea0db4cb09f31cbff3169e329918
107zxz <i107zxz@icloud.com>
2023-01-08 00:49:55 +1030

    Implemented Depleting

---
+++ scripts/classes/sigils/Depleting.gd
@@ -0,0 +1,18 @@
+extends SigilEffect
+
+# This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
+func handle_event(event: String, params: Array):
+
+	# attached_card_summoned represents the card bearing the sigil being summoned
+	if event == "card_summoned" and params[0] == card:
+		
+		
+#		if (fightManager.max_energy if isFriendly else fightManager.opponent_max_energy) < 2:
+#			return
+		
+		if isFriendly:
+			fightManager.set_max_energy(min(fightManager.max_energy - 2, 6))
+			fightManager.set_energy(min(fightManager.max_energy + fightManager.max_energy_buff, fightManager.energy))
+		else:
+			fightManager.set_opponent_max_energy(min(fightManager.opponent_max_energy - 2, 6))
+			fightManager.set_opponent_energy(min(fightManager.opponent_max_energy + fightManager.opponent_max_energy_buff, fightManager.opponent_energy))
