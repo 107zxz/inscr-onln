8e08b72af1f9236e203f948e6d99e4bf77532f5e
Thomas Robinson <i107zxz@icloud.com>
2023-06-27 10:43:50 +0930

    Added amalgam

--- scripts/classes/sigils/Scavenger.gd
+++ scripts/classes/sigils/Scavenger.gd
@@ -5,7 +5,6 @@ func handle_event(event: String, params: Array):
 
 	if event == "card_perished" and not card.in_hand and (params[0].get_parent().get_parent().name == "PlayerSlots") != isFriendly:
 		
-		
 		if isFriendly:
 			fightManager.add_bones(1)
 		else:
