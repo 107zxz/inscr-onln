00817e0226924f7e51cbafb369e698514b7001dd
Thomas Robinson <i107zxz@icloud.com>
2023-07-02 22:05:02 +0930

    Corrected Steel Trap, added Pelts

--- scripts/classes/sigils/Steel Trap.gd
+++ scripts/classes/sigils/Steel Trap.gd
@@ -4,8 +4,28 @@ extends SigilEffect
 func handle_event(event: String, params: Array):
 
 	# Did this card just get hit?
-	if event == "card_hit" and params[0] == card:
+	if event == "card_perished" and params[0] == card:
 		
-		if params[0].health <= 0 and params[1].is_alive() and not params[1].has_sigil("Made of Stone"):
-			print("Steel Trap triggered!")
-			params[1].get_node("AnimationPlayer").play("Perish")
+		print("Steel Trap Triggered!")
+		
+		var slot_idx = card.slot_idx()
+		
+		var target = slotManager.get_enemy_card(slot_idx) if isFriendly else slotManager.get_friendly_card(slot_idx)
+		
+		if not target:
+			return
+		
+		var pelt_name = "Wolf Pelt"
+		
+		# Which pelt to give
+		if "rare" in target.card_data:
+			pelt_name = "Golden Pelt"
+		elif target.card_data.attack == 0:
+			pelt_name = "Rabbit Pelt"
+		
+		# Kill it
+		target.get_node("AnimationPlayer").play("Perish")
+		
+		if not isFriendly:
+			# Draw the rabbit
+			fightManager.draw_card(CardInfo.from_name(pelt_name))
