955ea22b7e2df710312c4c764be571212d0fe4db
senor-huevo <137343393+senor-huevo@users.noreply.github.com>
2025-03-06 03:12:55 -0500

    Add files via upload

--- scripts/classes/cards/PlayingCard.gd
+++ scripts/classes/cards/PlayingCard.gd
@@ -791,15 +791,11 @@ func calculate_buffs():
 				for ant in slotManager.all_friendly_cards() if friendly else slotManager.all_enemy_cards():
 					if "Ant" in ant.card_data["name"] and "ant_limit" in CardInfo.all_data and attack < CardInfo.all_data.ant_limit:
 						attack += 1
-
-	# Bell Tentacle
-	if card_data["name"] == "Bell Tentacle":
-		attack = 4 - sIdx
-
-	# Hand Tentacle
-	if card_data["name"] == "Hand Tentacle":
-		var hName = "PlayerHand" if friendly else "EnemyHand"
-		attack = fightManager.get_node("HandsContainer/Hands/" + hName).get_child_count()
+			"Bell":
+				attack = 4 - sIdx
+			"Hand":
+				var hName = "PlayerHand" if friendly else "EnemyHand"
+				attack = fightManager.get_node("HandsContainer/Hands/" + hName).get_child_count()
 
 	# Conduits
 	var cfx = slotManager.get_conduitfx(self)
