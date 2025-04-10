5e89c0944d5862a446c6da6297135169b90df3f2
??? ??? <whiterobot210@gmail.com>
2025-04-10 12:40:56 -0400

    Fixed omni-strike
    I accidently forgot to change a + to a -, meaning omni strike would attack two times in the card's lane if there was nothing there

--- scripts/classes/sigils/Omni Strike.gd
+++ scripts/classes/sigils/Omni Strike.gd
@@ -5,7 +5,7 @@ func handle_event(event: String, params: Array):
 	pass
 	
 func modify_attack_targeting(index: int, strikes: Array):
-	strikes[index] += 1
+	strikes[index] -= 1
 	for eCard in card.all_enemy_cards():
 		strikes[eCard.slot_idx()] = 1
 	if not 1 in strikes:
