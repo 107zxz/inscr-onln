1a9b035e9e3b47d023d632c185e6e91251fc9a57
??? ??? <whiterobot210@gmail.com>
2025-04-10 12:44:13 -0400

    Removed unneccessary functions
    I forgot to remove some functions that override a function that does literally nothing with a function that also does literally nothing

--- scripts/classes/sigils/Bifurcated Strike.gd
+++ scripts/classes/sigils/Bifurcated Strike.gd
@@ -1,9 +1,6 @@
 extends SigilEffect
 
 # This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
-func handle_event(event: String, params: Array):
-	pass
-	
 func modify_attack_targeting(index: int, strikes: Array):
 	strikes[index+1] += 1
 	strikes[index] -= 1
