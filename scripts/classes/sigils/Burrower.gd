68cee9477714173a9e7ac8fc7ce3097e83463a68
??? ??? <whiterobot210@gmail.com>
2025-04-11 22:48:16 -0400

    Fixed burrower
    forgot a not when copy and pasting

--- scripts/classes/sigils/Burrower.gd
+++ scripts/classes/sigils/Burrower.gd
@@ -3,7 +3,7 @@ extends SigilEffect
 #now, instead of a bunch of hardcoded bullshit, we just simulate what would (probably) happen if the burrower moved to the slot, and if it would intercept, it moves.
 func pre_enemy_attack(attacker, targeted_index: int, current_targeting):
 	if current_targeting == AttackTargeting.SCALE:
-		if (isFriendly and slotManager.is_slot_empty(slotManager.playerSlots[targeted_index])) or (isFriendly and slotManager.is_slot_empty(slotManager.enemySlots[targeted_index])):
+		if (isFriendly and slotManager.is_slot_empty(slotManager.playerSlots[targeted_index])) or (not isFriendly and slotManager.is_slot_empty(slotManager.enemySlots[targeted_index])):
 			var targeting_test = AttackTargeting.CARD
 			for sig in attacker.sigils:
 				targeting_test = sig.attacker_target_selecting(targeting_test, card)
