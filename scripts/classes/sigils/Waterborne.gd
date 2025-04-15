16cf6ff2a2524c775c38ea141827f1da445cd8c8
??? ??? <whiterobot210@gmail.com>
2025-04-14 21:10:45 -0400

    Bellist
    Bellists are now properly forbidden from attacking anything that functions like Repulsive but itself isn't. No such things currently exist, but hey, futureproofing.
    
    Waterborne changed such that it only stops attacks if the dive overlay is visible. Now that I think about it, this actually doesn't matter for bellists.

--- scripts/classes/sigils/Waterborne.gd
+++ scripts/classes/sigils/Waterborne.gd
@@ -2,7 +2,7 @@ extends SigilEffect
 
 func defender_target_selecting(current_targeting, attacking_card):
 	# Aquanaut unfortunately cannot be added normally, so to impliment it, simply add: `and not attacking_card.has_sigil("Aquanaut")`
-	if current_targeting == AttackTargeting.CARD:
+	if current_targeting == AttackTargeting.CARD and card.get_node("CardBody/DiveOlay").visible:
 		return AttackTargeting.SCALE
 	return current_targeting
 
