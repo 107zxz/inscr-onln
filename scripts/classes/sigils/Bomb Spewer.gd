3c8b4781738c3e9b226c7d9c4fa954cf9f4ebdbe
NubbyBoi <147878686+NubbyBoi@users.noreply.github.com>
2025-04-24 21:15:24 +0100

    fix Bomb Spewers
    i missed another sigil (oops)
    Bomb Spewer should now also works when `lanes!=4`

--- scripts/classes/sigils/Bomb Spewer.gd
+++ scripts/classes/sigils/Bomb Spewer.gd
@@ -6,7 +6,7 @@ func handle_event(event: String, params: Array):
 	# attached_card_summoned represents the card bearing the sigil being summoned
 	if event == "card_summoned" and params[0] == card:
 		if not "DoublePerish" in card.get_node("AnimationPlayer").current_animation:
-			for cSlot in range(4):
+			for cSlot in range(CardInfo.all_data.lanes):
 				if slotManager.is_slot_empty(slotManager.playerSlots[cSlot]):
 					slotManager.summon_card(CardInfo.from_name("Explode Bot"), cSlot, true)
 					print("Summoning boombot into friendly slot ", cSlot)
