02b1d44d3a0810565d2370cf89cfe451ccdaeb92
senor-huevo <137343393+senor-huevo@users.noreply.github.com>
2025-05-01 15:33:49 -0400

    Gem Detonator (5) and Gem Dependant fix
    Fixes Gem Detonator to count itself, as requested by SylveonM45T3R.
    
    Adds customization to Gem Dep to work how it does in base game;

--- scripts/classes/sigils/Gem Dependant.gd
+++ scripts/classes/sigils/Gem Dependant.gd
@@ -1,17 +1,20 @@
 extends SigilEffect
 
+func start_of_turn(cardAnim):
+	Gem_Dep()
+	
 # This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
 func handle_event(event: String, params: Array):
 
-	# Don't die in hand
-	if card.get_parent().name == "PlayerHand":
-		return
-
 	# attached_card_summoned represents the card bearing the sigil being summoned
-	if (event == "card_summoned" and params[0] == card) or (event == "card_perished" and params[0] != card):
+	if (event == "card_summoned" and params[0] == card and not card.in_hand) or (event == "card_perished" and params[0] != card and not card.in_hand and not "vanilla_gem_dep" in CardInfo.all_data):
+			Gem_Dep()
 
-		# Die if no gems
-		if not "Perish" in card.get_node("AnimationPlayer").current_animation:
+func end_of_turn(cardAnim):
+	Gem_Dep()
+	
+func Gem_Dep():
+	if not "Perish" in card.get_node("AnimationPlayer").current_animation:
 
 			var kill = not (slotManager.get_friendly_cards_sigil("Great Mox") if isFriendly else slotManager.get_enemy_cards_sigil("Great Mox"))
 
@@ -24,4 +27,3 @@ func handle_event(event: String, params: Array):
 			if kill:
 				print("Gem dependant card should die!")
 				card.get_node("AnimationPlayer").play("Perish")
-		
