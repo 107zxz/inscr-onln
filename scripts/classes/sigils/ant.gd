ec1b4ef63c1cc18700ae183eb32e39fe5a3e8e11
??? ??? <whiterobot210@gmail.com>
2025-05-12 01:14:08 -0400

    Oopsies...
    Forgot to remove is_power_defining() when it's no longer the method for using power defining sigils.
    
    Added the important comment information to all sigls that use the define_power() function that explains how you use sigils with that code

--- scripts/classes/sigils/ant.gd
+++ scripts/classes/sigils/ant.gd
@@ -1,6 +1,8 @@
 extends SigilEffect
 
 #Used for sigils that passively define the power of the card they're attached to, such as Ant, Spilled Blood, etc...
+#IMPORTANT! Sigils with this effect do not go with normal sigils, they must be put in the 'atkspecial' arguement.
+#Note that sigils in 'atkspecial' will be excluded from the normal sigil list(s), meaning none of their other functions will trigger.
 func define_power():
 	var attack = card.card_data.attack
 	for ant in slotManager.all_friendly_cards() if is_friendly else slotManager.all_enemy_cards():
--- scripts/classes/sigils/mirror.gd
+++ scripts/classes/sigils/mirror.gd
@@ -1,6 +1,8 @@
 extends SigilEffect
 
 #Used for sigils that passively define the power of the card they're attached to, such as Ant, Spilled Blood, etc...
+#IMPORTANT! Sigils with this effect do not go with normal sigils, they must be put in the 'atkspecial' arguement.
+#Note that sigils in 'atkspecial' will be excluded from the normal sigil list(s), meaning none of their other functions will trigger.
 func define_power():
 	var sIdx = card.slot_idx()
 	if is_friendly:
@@ -9,7 +11,3 @@ func define_power():
 	else:
 		if slotManager.get_friendly_card(sIdx):
 			return slotManager.get_friendly_card(sIdx).attack
-
-#IMPORTANT: replace this with 'return true' for any sigil that sets the user's Power/Attack. Returns false otherwise, as a card can only have one of these at once. 
-func is_power_defining():
-	return true
